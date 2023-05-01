d38bf85fa8f1da9bf4153438b7df1bce9838de80
Roarke-Rey <shreyas992010@gmail.com>
2023-04-30 22:22:36 -0400

    Added the map fragment and the information sharing between map and gallery fragment

--- app/src/main/java/edu/vt/cs5254/fancygallery/GalleryFragment.kt
+++ app/src/main/java/edu/vt/cs5254/fancygallery/GalleryFragment.kt
@@ -1,11 +1,10 @@
 package edu.vt.cs5254.fancygallery
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -13,12 +12,8 @@ import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import coil.imageLoader
-import edu.vt.cs5254.fancygallery.api.FlickrApi
 import edu.vt.cs5254.fancygallery.databinding.FragmentGalleryBinding
 import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
-import retrofit2.create
 
 private const val TAG = "PhotoGalleryFragment"
 
@@ -30,7 +25,7 @@ class GalleryFragment: Fragment() {
             "Cannot access binding because it is null. Is the view visible?"
         }
 
-    private val galleryViewModel: GalleryViewModel by viewModels()
+    private val mainViewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -49,7 +44,7 @@ class GalleryFragment: Fragment() {
                 return when (menuItem.itemId){
                     R.id.reload_menu -> {
                         requireContext().imageLoader.memoryCache?.clear()
-                        galleryViewModel.reloadGalleryItems()
+                        mainViewModel.reloadGalleryItems()
                         true
                     }
                     else -> false
@@ -66,7 +61,7 @@ class GalleryFragment: Fragment() {
 
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                galleryViewModel.galleryItems.collect { items ->
+                mainViewModel.galleryItems.collect { items ->
                     binding.photoGrid.adapter = GalleryItemAdapter(
                         items
                     ) { photoPageUri ->
--- app/src/main/java/edu/vt/cs5254/fancygallery/MainViewModel.kt
+++ app/src/main/java/edu/vt/cs5254/fancygallery/MainViewModel.kt
@@ -9,9 +9,9 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
-private const val TAG = "GalleryViewModel"
+private const val TAG = "MainViewModel"
 
-class GalleryViewModel: ViewModel() {
+class MainViewModel: ViewModel() {
     private val photoRepository = PhotoRepository()
 
     private val _galleryItems: MutableStateFlow<List<GalleryItem>> =
